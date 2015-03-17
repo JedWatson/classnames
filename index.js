4e25eba624bceb8b5449a62033261cd442b178e6
Daniel Streit <DanielS@clearstorydata.com>
2015-03-17 06:02:20 -0700

    Adds support for Arrays and removes use of Array#join

--- index.js
+++ index.js
@@ -1,24 +1,27 @@
 function classNames() {
-	var args = arguments;
-	var classes = [];
+	var classes = '';
+	var arg;
 
-	for (var i = 0; i < args.length; i++) {
-		var arg = args[i];
+	for (var i = 0; i < arguments.length; i++) {
+		arg = arguments[i];
 		if (!arg) {
 			continue;
 		}
 
 		if ('string' === typeof arg || 'number' === typeof arg) {
-			classes.push(arg);
+			classes += ' ' + arg;
+		} else if (Object.prototype.toString.call(arg) === '[object Array]') {
+			classes += ' ' + classNames.apply(null, arg);
 		} else if ('object' === typeof arg) {
 			for (var key in arg) {
-				if (arg.hasOwnProperty(key) && arg[key]) {
-					classes.push(key);
+				if (!arg.hasOwnProperty(key) || !arg[key]) {
+					continue;
 				}
+				classes += ' ' + key;
 			}
 		}
 	}
-	return classes.join(' ');
+	return classes.substr(1);
 }
 
 // safely export classNames in case the script is included directly on a page
