c860cbbd4498d4f0fbc25de226dc768eaad955e8
Daniel Cousens <github@dcousens.com>
2015-05-04 18:38:16 +1000

    index: declare var in loop

--- index.js
+++ index.js
@@ -8,23 +8,22 @@ function classNames () {
 	'use strict';
 
 	var classes = '';
-	var arg;
 
 	for (var i = 0; i < arguments.length; i++) {
-		arg = arguments[i];
-		if (!arg) {
-			continue;
-		}
+		var arg = arguments[i];
+		if (!arg) continue;
+
 		if ('string' === typeof arg || 'number' === typeof arg) {
 			classes += ' ' + arg;
+
 		} else if (Array.isArray(arg)) {
 			classes += ' ' + classNames.apply(null, arg);
+
 		} else if ('object' === typeof arg) {
 			for (var key in arg) {
-				if (!arg.hasOwnProperty(key) || !arg[key]) {
-					continue;
+				if (arg.hasOwnProperty(key) && arg[key]) {
+					classes += ' ' + key;
 				}
-				classes += ' ' + key;
 			}
 		}
 	}
