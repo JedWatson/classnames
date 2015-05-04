c3a03a2391d720689f66a66d0b06791a6ec18f60
Jed Watson <jed.watson@me.com>
2015-05-04 14:26:46 +1000

    Consistency and other fixes for dedupe, closes #31

--- dedupe.js
+++ dedupe.js
@@ -1,63 +1,69 @@
-'use strict';
+/*!
+  Copyright (c) 2015 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
 
-var SPACE = /\s+/;
-var toString = Object.prototype.toString;
+var classNames = (function () {
+	'use strict';
 
-function _parse (result, arg) {
-	if (!arg) {
-		return;
-	}
+	var SPACE = /\s+/;
+	var toString = Object.prototype.toString;
 
-	if ('number' === typeof arg) {
-		result[arg] = true;
-	} else if ('string' === typeof arg) {
-		arg = arg.split(SPACE);
-		if (arg.length === 1) {
-			result[arg[0]] = true;
-		} else if (arg.length) {
+	function _parse (result, arg) {
+		if (!arg) {
+			return;
+		}
+		if ('number' === typeof arg) {
+			result[arg] = true;
+		} else if ('string' === typeof arg) {
+			arg = arg.split(SPACE);
+			if (arg.length === 1) {
+				result[arg[0]] = true;
+			} else if (arg.length) {
+				for (var i = 0; i < arg.length; i++) {
+					result[arg[i]] = true;
+				}
+			}
+		} else if (Array.isArray(arg)) {
 			for (var i = 0; i < arg.length; i++) {
-				result[arg[i]] = true;
+				_parse(result, arg[i]);
 			}
-		}
-	} else if (toString.call(arg) === '[object Array]') {
-		for (var i = 0; i < arg.length; i++) {
-			_parse(result, arg[i]);
-		}
-	} else if ('object' === typeof arg) {
-		for (var k in arg) {
-			if (arg.hasOwnProperty(k)) {
-				result[k] = arg[k];
+		} else if ('object' === typeof arg) {
+			for (var k in arg) {
+				if (arg.hasOwnProperty(k)) {
+					result[k] = arg[k];
+				}
 			}
 		}
 	}
 
-}
-
-function dedupe () {
-	var classSet = {};
-	var classes = '';
-
-	for (var i = 0, arg = arguments[i]; i < arguments.length; arg = arguments[++i]) {
-		_parse(classSet, arg);
-	}
-
-	for (var k in classSet) {
-		if (classSet.hasOwnProperty(k) && classSet[k]) {
-			classes += ' ' + k;
+	function _classNames () {
+		var classSet = {};
+		var classes = '';
+		for (var i = 0, arg = arguments[i]; i < arguments.length; arg = arguments[++i]) {
+			_parse(classSet, arg);
 		}
+		for (var k in classSet) {
+			if (classSet.hasOwnProperty(k) && classSet[k]) {
+				classes += ' ' + k;
+			}
+		}
+		return classes.substr(1);
 	}
 
-	return classes.substr(1);
-}
+	return _classNames;
 
-// safely export dedupe for node / browserify
+})();
+
+// safely export classNames for node / browserify
 if (typeof module !== 'undefined' && module.exports) {
-	module.exports = dedupe;
+	module.exports = classNames;
 }
 
-// safely export dedupe for RequireJS
+// safely export classNames for RequireJS
 if (typeof define !== 'undefined' && define.amd) {
-	define('dedupe', [], function() {
-		return dedupe;
+	define('classNames', [], function() {
+		return classNames;
 	});
 }
