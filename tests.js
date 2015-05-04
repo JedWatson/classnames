e5680d596d7059cc7f352c885eabda7405fa63fb
Daniel Cousens <github@dcousens.com>
2015-05-04 18:21:11 +1000

    tests: just use assert.equal

--- tests.js
+++ tests.js
@@ -64,15 +64,6 @@ describe('classNames', function() {
 });
 
 describe('dedupe', function () {
-	function _hasSameClasses (c1, c2) {
-		if (c1 === c2) return true
-
-		c1 = c1.split(SPACE).sort().join(' ');
-		c2 = c2.split(SPACE).sort().join(' ');
-
-		return c1 === c2
-	}
-
 	it('keeps object keys with truthy values', function() {
 		assert.equal(dedupe({
 			a: true,
@@ -85,62 +76,62 @@ describe('dedupe', function () {
 	});
 
 	it('should dedupe dedupe', function () {
-		assert(_hasSameClasses(dedupe('foo', 'bar', 'foo', 'bar', { foo: true }), 'foo bar'));
+		assert.equal(dedupe('foo', 'bar', 'foo', 'bar', { foo: true }), 'foo bar');
 	});
 
 	it('should make sure subsequent objects can remove/add classes', function () {
-		assert(_hasSameClasses(dedupe('foo', { foo: false }, { foo: true, bar: true }), 'foo bar'));
+		assert.equal(dedupe('foo', { foo: false }, { foo: true, bar: true }), 'foo bar');
 	});
 
 	it('should make sure object with falsy value wipe out previous classes', function () {
-		assert(_hasSameClasses(dedupe('foo foo', 0, null, undefined, true, 1, 'b', { 'foo': false }), '1 b'));
-		assert(_hasSameClasses(dedupe("foo", "foobar", "bar", { foo: false }), "foobar bar"));
-		assert(_hasSameClasses(dedupe("foo", "foo-bar", "bar", { foo: false }), "foo-bar bar"));
-		assert(_hasSameClasses(dedupe("foo", "-moz-foo-bar", "bar", { foo: false }), "-moz-foo-bar bar"));
+		assert.equal(dedupe('foo foo', 0, null, undefined, true, 1, 'b', { 'foo': false }), '1 b');
+		assert.equal(dedupe("foo", "foobar", "bar", { foo: false }), "foobar bar");
+		assert.equal(dedupe("foo", "foo-bar", "bar", { foo: false }), "foo-bar bar");
+		assert.equal(dedupe("foo", "-moz-foo-bar", "bar", { foo: false }), "-moz-foo-bar bar");
 	});
 
 	it('joins arrays of class names and ignore falsy values', function() {
-		assert(_hasSameClasses(dedupe('a', 0, null, undefined, true, 1, 'b'), 'a 1 b'));
+		assert.equal(dedupe('a', 0, null, undefined, true, 1, 'b'), '1 a b');
 	});
 
 	it('supports heterogenous arguments', function() {
-		assert(_hasSameClasses(dedupe({a: true}, 'b', 0), 'a b'));
+		assert.equal(dedupe({a: true}, 'b', 0), 'a b');
 	});
 
 	it('should be trimmed', function() {
-		assert(_hasSameClasses(dedupe('', 'b', {}, ''), 'b'));
+		assert.equal(dedupe('', 'b', {}, ''), 'b');
 	});
 
 	it('returns an empty string for an empty configuration', function() {
-		assert(_hasSameClasses(dedupe({}), ''));
+		assert.equal(dedupe({}), '');
 	});
 
 	it('supports an array of class names', function() {
-		assert(_hasSameClasses(dedupe(['a', 'b']), 'a b'));
+		assert.equal(dedupe(['a', 'b']), 'a b');
 	});
 
 	it('joins array arguments with string arguments', function() {
-		assert(_hasSameClasses(dedupe(['a', 'b'], 'c'), 'a b c'));
-		assert(_hasSameClasses(dedupe('c', ['a', 'b']), 'c a b'));
+		assert.equal(dedupe(['a', 'b'], 'c'), 'a b c');
+		assert.equal(dedupe('c', ['a', 'b']), 'c a b');
 	});
 
 	it('handles multiple array arguments', function() {
-		assert(_hasSameClasses(dedupe(['a', 'b'], ['c', 'd']), 'a b c d'));
+		assert.equal(dedupe(['a', 'b'], ['c', 'd']), 'a b c d');
 	});
 
 	it('handles arrays that include falsy and true values', function() {
-		assert(_hasSameClasses(dedupe(['a', 0, null, undefined, false, true, 'b']), 'a b'));
+		assert.equal(dedupe(['a', 0, null, undefined, false, true, 'b']), 'a b');
 	});
 
 	it('handles arrays that include arrays', function() {
-		assert(_hasSameClasses(dedupe(['a', ['b', 'c']]), 'a b c'));
+		assert.equal(dedupe(['a', ['b', 'c']]), 'a b c');
 	});
 
 	it('handles arrays that include objects', function() {
-		assert(_hasSameClasses(dedupe(['a', {b: true, c: false}]), 'a b'));
+		assert.equal(dedupe(['a', {b: true, c: false}]), 'a b');
 	});
 
 	it('handles deep array recursion', function() {
-		assert(_hasSameClasses(dedupe(['a', ['b', ['c', {d: true}]]]), 'a b c d'));
+		assert.equal(dedupe(['a', ['b', ['c', {d: true}]]]), 'a b c d');
 	});
 });
