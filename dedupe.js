2c3f46254044ccc9f13d15afe3e7318d21d085b9
Davi Ferreira <contato@daviferreira.com>
2015-05-28 10:41:35 +0200

    Add umd wrapper

--- dedupe.js
+++ dedupe.js
@@ -4,91 +4,92 @@
   http://jedwatson.github.io/classnames
 */
 
-var classNames = (function () {
+(function () {
 	'use strict';
 
-	function _parseArray (resultSet, array) {
-		var length = array.length;
+	var classNames = (function () {
+		function _parseArray (resultSet, array) {
+			var length = array.length;
 
-		for (var i = 0; i < length; ++i) {
-			_parse(resultSet, array[i]);
+			for (var i = 0; i < length; ++i) {
+				_parse(resultSet, array[i]);
+			}
 		}
-	}
 
-	function _parseNumber (resultSet, num) {
-		resultSet[num] = true;
-	}
+		function _parseNumber (resultSet, num) {
+			resultSet[num] = true;
+		}
 
-	function _parseObject (resultSet, object) {
-		for (var k in object) {
-			if (object.hasOwnProperty(k)) {
-				if (object[k]) {
-					resultSet[k] = true;
+		function _parseObject (resultSet, object) {
+			for (var k in object) {
+				if (object.hasOwnProperty(k)) {
+					if (object[k]) {
+						resultSet[k] = true;
 
-				} else {
-					delete resultSet[k];
+					} else {
+						delete resultSet[k];
+					}
 				}
 			}
 		}
-	}
 
-	var SPACE = /\s+/;
-	function _parseString (resultSet, str) {
-		var array = str.split(SPACE);
-		var length = array.length;
+		var SPACE = /\s+/;
+		function _parseString (resultSet, str) {
+			var array = str.split(SPACE);
+			var length = array.length;
 
-		for (var i = 0; i < length; ++i) {
-			resultSet[array[i]] = true;
-		}
-	}
-
-	function _parse (resultSet, arg) {
-		if (!arg) return;
-		var argType = typeof arg;
-
-		// 'foo bar'
-		if ('string' === argType) {
-			_parseString(resultSet, arg);
-
-		// ['foo', 'bar', ...]
-		} else if (Array.isArray(arg)) {
-			_parseArray(resultSet, arg);
-
-		// { 'foo': true, ... }
-		} else if ('object' === argType) {
-			_parseObject(resultSet, arg);
-
-		// '130'
-		} else if ('number' === argType) {
-			_parseNumber(resultSet, arg);
-		}
-	}
-
-	function _classNames () {
-		var classSet = {};
-		_parseArray(classSet, arguments);
-
-		var classes = '';
-		for (var k in classSet) {
-			classes += ' ' + k;
+			for (var i = 0; i < length; ++i) {
+				resultSet[array[i]] = true;
+			}
 		}
 
-		return classes.substr(1);
+		function _parse (resultSet, arg) {
+			if (!arg) return;
+			var argType = typeof arg;
+
+			// 'foo bar'
+			if ('string' === argType) {
+				_parseString(resultSet, arg);
+
+			// ['foo', 'bar', ...]
+			} else if (Array.isArray(arg)) {
+				_parseArray(resultSet, arg);
+
+			// { 'foo': true, ... }
+			} else if ('object' === argType) {
+				_parseObject(resultSet, arg);
+
+			// '130'
+			} else if ('number' === argType) {
+				_parseNumber(resultSet, arg);
+			}
+		}
+
+		function _classNames () {
+			var classSet = {};
+			_parseArray(classSet, arguments);
+
+			var classes = '';
+			for (var k in classSet) {
+				classes += ' ' + k;
+			}
+
+			return classes.substr(1);
+		}
+
+		return _classNames;
+
+	})();
+
+	if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
+		// AMD. Register as an anonymous module.
+		define(function () {
+			return classNames;
+		});
+	} else if (typeof module !== 'undefined' && module.exports) {
+		module.exports = classNames;
+	} else {
+		window.classNames = classNames;
 	}
 
-	return _classNames;
-
-})();
-
-// safely export classNames for node / browserify
-if (typeof module !== 'undefined' && module.exports) {
-	module.exports = classNames;
-}
-
-/* global define */
-// safely export classNames for RequireJS
-if (typeof define !== 'undefined' && define.amd) {
-	define('classNames', [], function() {
-		return classNames;
-	});
-}
+}());
