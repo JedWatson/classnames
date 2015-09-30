fc4cdb54efde0aede2c99bf0d7be210f689db5d1
Daniel Cousens <github@dcousens.com>
2015-09-30 10:00:57 +1000

    benchmarks: add common case

--- benchmarks/run.js
+++ benchmarks/run.js
@@ -10,6 +10,11 @@ var fixtures = [
 		expected: 'one two'
 	},
 	{
+		description: 'strings, object',
+		args: ['one', 'two', { four: true, three: false }],
+		expected: 'one two four'
+	},
+	{
 		description: 'mix',
 		args: ['one', { two: true, three: false }, { four: 'four', five: true }, 6, {}],
 		expected: 'one two four five 6'
