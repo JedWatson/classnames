915186aaa510e0c6be6c2cf20788aac474790e27
Jed Mao <jedmao@outlook.com>
2016-12-10 01:48:55 -0600

    ES Module Export

--- index.js
+++ index.js
@@ -36,6 +36,7 @@
 	}
 
 	if (typeof module !== 'undefined' && module.exports) {
+		classNames.default = classNames;
 		module.exports = classNames;
 	} else if (typeof define === 'function' && typeof define.amd === 'object' && define.amd) {
 		// register as 'classnames', consistent with npm package name
