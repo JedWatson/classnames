f23da0621489d7cc9c9c1d2e1c093493ec9a69bd
Jed Mao <jedmao@outlook.com>
2018-04-24 21:33:42 -0500

    Add tests for TypeScript import

---
+++ tests/index.ts
@@ -0,0 +1,8 @@
+import * as assert from 'assert';
+import classNames from '../';
+
+describe('classNames module', function() {
+	it('exports a function as default', function() {
+		assert.equal(typeof classNames, 'function');
+	});
+});
