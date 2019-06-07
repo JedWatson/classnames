c3a41a97f59ca0b23f3284677e12b01156db1c79
Jason Miller <developit@google.com>
2019-06-07 17:05:50 -0400

    Add src directory

---
+++ src/bind.js
@@ -0,0 +1,33 @@
+/*!
+  Copyright (c) 2018 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+/* global define */
+
+var hasOwn = {}.hasOwnProperty;
+
+export default function classNames () {
+	var classes = [];
+
+	for (var i = 0; i < arguments.length; i++) {
+		var arg = arguments[i];
+		if (!arg) continue;
+
+		var argType = typeof arg;
+
+		if (argType === 'string' || argType === 'number') {
+			classes.push(this && this[arg] || arg);
+		} else if (Array.isArray(arg)) {
+			classes.push(classNames.apply(this, arg));
+		} else if (argType === 'object') {
+			for (var key in arg) {
+				if (hasOwn.call(arg, key) && arg[key]) {
+					classes.push(this && this[key] || key);
+				}
+			}
+		}
+	}
+
+	return classes.join(' ');
+}
---
+++ src/index.js
@@ -0,0 +1,35 @@
+/*!
+  Copyright (c) 2018 Jed Watson.
+  Licensed under the MIT License (MIT), see
+  http://jedwatson.github.io/classnames
+*/
+
+var hasOwn = {}.hasOwnProperty;
+
+export default function classNames () {
+	var classes = [];
+
+	for (var i = 0; i < arguments.length; i++) {
+		var arg = arguments[i];
+		if (!arg) continue;
+
+		var argType = typeof arg;
+
+		if (argType === 'string' || argType === 'number') {
+			classes.push(arg);
+		} else if (Array.isArray(arg) && arg.length) {
+			var inner = classNames.apply(null, arg);
+			if (inner) {
+				classes.push(inner);
+			}
+		} else if (argType === 'object') {
+			for (var key in arg) {
+				if (hasOwn.call(arg, key) && arg[key]) {
+					classes.push(key);
+				}
+			}
+		}
+	}
+
+	return classes.join(' ');
+}
