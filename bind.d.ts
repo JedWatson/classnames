dec702ac2523157cab7c81da9e35e851ef0e562b
Remco Haszing <remcohaszing@gmail.com>
2021-05-08 16:46:20 +0200

    Add TypeScript tests using dtslint
    This asserts the types work in TypeScript 3.0 and above. Some issues were
    uncovered by the tests. These have been fixed.

--- bind.d.ts
+++ bind.d.ts
@@ -1,5 +1,5 @@
-export * from './index';
-import { Argument } from './index';
+export * from '.';
+import { ArgumentArray } from '.';
 
-export type Binding = { [key: string]: string };
-export default function classNames(this: Binding | void, ...args: Argument[]): string;
+export type Binding = Record<string, string>;
+export default function classNames(this: Binding | undefined, ...args: ArgumentArray): string;
